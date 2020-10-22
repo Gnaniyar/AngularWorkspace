e02a5f876de9231981f856f8a5fbe1eb6a14609a
Gnaniyar Sainudeen <ssgnaniyar@gmail.com>
2020-10-22 15:30:45 +0530

    delimeter menu display changes

--- src/app/file-upload-layout/file-upload-layout.component.ts
+++ src/app/file-upload-layout/file-upload-layout.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-upload-layout.component.css']
 })
 export class FileUploadLayoutComponent implements OnInit {
-
+  showDelimeterMenu = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleDelimeterMenu(): void {
+    this.showDelimeterMenu = !this.showDelimeterMenu;
+  }
 }
